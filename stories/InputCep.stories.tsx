89b38fccc7751117df5bc3fd0bd20840ec2237c2
carlin <carlin@pop-os.localdomain>
2023-07-20 23:32:59 -0300

    Inputs storyBook

---
+++ stories/InputCep.stories.tsx
@@ -0,0 +1,9 @@
+import { Meta, StoryObj } from "@storybook/react";
+import { CepInput } from "../app/Components/Input/InputCep";
+
+export default {
+  title: "Components/InputCep",
+  component: CepInput,
+} as Meta;
+
+export const InputCep: StoryObj = {};
